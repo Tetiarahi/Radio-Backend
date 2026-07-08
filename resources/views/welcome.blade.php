be29aa5b41838349e5df49c6c847a629932928c5
Tetiarahi Mathew <itm@bpa.org.ki>
2026-07-08 16:02:14 +1200

    Update welcome view

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,7 +21,7 @@
                 <svg class="w-6 h-6 text-[#f53003] dark:text-[#FF4433]" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19.114 5.636a9 9 0 010 12.728M16.463 8.288a5.25 5.25 0 010 7.424M6.75 8.25l4.72-4.72a.75.75 0 011.28.53v15.88a.75.75 0 01-1.28.53l-4.72-4.72H4.51c-.88 0-1.704-.507-1.938-1.354A9.01 9.01 0 012.25 12c0-.83.112-1.633.322-2.396C2.806 8.756 3.63 8.25 4.51 8.25H6.75z" />
                 </svg>
-                {{ config('app.name', 'Radio') }}
+                {{-- {{ config('app.name', 'Radio') }} --}}Radio Kiribati
             </a>
             <nav class="flex items-center gap-3">
                 @if (Route::has('admin.login'))
